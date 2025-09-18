75cd388b918640fee45faf8ac4e050f3c078e91c
IvanCastellanos21 <124224052+IvanCastellanos21@users.noreply.github.com>
2025-09-18 10:11:35 -0600

    Add files via upload

---
+++ 18092025/app 3.js
@@ -0,0 +1,26 @@
+// var miCarro = new Object();
+// miCarro.marca = 'Toyota';
+// miCarro.marca = 'Supra';
+// miCarro.marca = 'Ferrari';
+
+// console.log(miCarro);
+
+// var miCarro = {
+//     marca: "Toyota",
+//     modelo: "Supra",
+//     color: "rojo",
+// }
+
+// console.log(miCarro);
+
+var perro = {
+    nombre: "Shayla",
+    color: "sal-pimienta",
+    edad: 20,
+    talla: "mediana",
+    enemigos: ["Otro perro", "Gatos", "Lobos"]
+}
+
+console.log(perro);
+console.log(perro.color);
+console.log(perro.enemigos[1]);
