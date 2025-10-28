e74af619ca0927d5bd30bedf5a5ec38505208d97
IvanCastellanos21 <124224052+IvanCastellanos21@users.noreply.github.com>
2025-10-28 10:52:56 -0600

    Add files via upload

---
+++ Inicio/app.js
@@ -0,0 +1,7 @@
+import os from 'node:os';
+
+console.log("Informacion de mi sistema");
+console.log("-------------------------");
+console.log("Nombre del sistema", os.platform());
+console.log("Version de mi sistema: ", os.release());
+console.log("Arquitectura", op.cpus());
